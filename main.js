06193a0e423150075e142c0bbfd5241eaf9580e9
Rishabh <rishabh.ramg@gmail.com>
2021-10-06 20:13:55 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,81 @@
+const addButton = document.querySelector( "#add" ) ;
+
+const updateLocalStorageData = () =>
+{
+  const textAreaData = document.querySelectorAll( "textarea" ) ;
+  const notes = [] ;
+
+  textAreaData.forEach( ( note ) =>
+  {
+    return notes.push( note.value ) ;
+  } ) ;
+
+  localStorage.setItem( "notes" , JSON.stringify( notes ) ) ;
+}
+
+const addNewNote = ( text = "" ) =>
+{
+  const note = document.createElement( "div" ) ;
+  note.classList.add( "note" ) ;
+
+  const htmlData = 
+  `
+    <div class="operation">
+    <button class="edit"><i class="fas fa-pencil-alt"></i></button>
+    <button class="delete"><i class="fas fa-trash"></i></button>
+    </div> 
+
+    <div class="main ${ text ? "" : "hidden" }"></div>
+    <textarea class="${ text ? "hidden" : "" }"></textarea>
+  ` ;
+
+  note.insertAdjacentHTML( "afterbegin" , htmlData ) ;
+  // console.log( note ) ;
+
+  // getting references
+  const editButton = note.querySelector( ".edit" ) ;
+  const deleteButton = note.querySelector( ".delete" ) ;
+  const mainDiv = note.querySelector( ".main" ) ;
+  const textArea = note.querySelector( "textarea" ) ;
+
+  // deleting a note
+  deleteButton.addEventListener( "click", () =>
+  {
+    note.remove() ;
+    updateLocalStorageData() ;
+  } ) ;
+
+  // toggle using edit button
+  textArea.value = text ;
+  mainDiv.innerHTML = text ;
+
+  editButton.addEventListener( "click", () =>
+  {
+    mainDiv.classList.toggle( "hidden" ) ;
+    textArea.classList.toggle( "hidden" ) ;
+  } ) ;
+
+  textArea.addEventListener( "change" , ( event ) =>
+  {
+    const value = event.target.value ;
+    mainDiv.innerHTML = value ;
+    // console.log( value ) ;
+
+    updateLocalStorageData() ;
+  } ) ;
+
+  document.body.appendChild( note ) ; // append after the last child
+}
+
+// fetching data from local storage
+const notes = JSON.parse( localStorage.getItem( "notes" ) ) ;
+
+if( notes )
+{
+  notes.forEach( ( note ) =>
+  {
+    addNewNote( note ) ;
+  } )
+}
+
+addButton.addEventListener( "click" , () => addNewNote() ) ;
